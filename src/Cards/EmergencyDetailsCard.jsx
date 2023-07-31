3b56f71d0f24e2c03d4b198ab57965e0c8464ba5
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-08-01 01:02:37 +0530

    Emergency Detail Card added

--- src/Cards/EmergencyDetailsCard.jsx
+++ src/Cards/EmergencyDetailsCard.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import "./EmergencyDetailsCard.css";
 
 const EmergencyDetailsCard = (props) => {
   return (
-    <div>
-      {props.data.map((data) => {
-        return (
-          <div>
-            <div> {data.name}</div>
-            <div> {data.email}</div>
-            <div> {data.contact}</div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="detailsContact">
+        <table className="table">
+          <thead className="thead">
+            <tr className="row">
+              <th className="col">Sr.No</th>
+              <th className="col">Name</th>
+              <th className="col">Email</th>
+              <th className="col">PhoneNo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.data.map((data) => {
+              return (
+                <tr key={data.id} className="row">
+                  <td className="column">{data.id}</td>
+                  <td className="column">{data.name}</td>
+                  <td className="column">{data.email}</td>
+                  <td className="column">{data.contact}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
