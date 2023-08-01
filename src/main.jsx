9ffc832a84f54b1c76f9f1f45b86f22acb7591ea
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-08-01 19:17:34 +0530

    Adding Login Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import LoginPage from './components/LoginPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoginPage />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
