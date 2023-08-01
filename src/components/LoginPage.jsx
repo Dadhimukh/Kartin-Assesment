0e39957d2d0d6db247a7fd9ae27b461bd9e64c71
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-08-01 19:18:22 +0530

    Updated login if the user is loged in

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import "./LoginPage.css"
+import React, { useState } from "react";
+import "./LoginPage.css";
+import HomePage from "./HomePage";
 
 const LoginPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="loginNavbar">
-      <div className="loginLogo">
-        <img
-          src="https://cdn-icons-png.flaticon.com/128/5969/5969117.png"
-          alt="company Logo"
-        />
-        <h1 className="loginLogoDetails">HLGS(Healthy Living Guide for Seniors)</h1>
-      </div>
-      <div className="loginSection">
-        <div className="loginInput">
-          <label htmlFor="">Email</label>
-          <input type="text" placeholder="Email" />
+    <div>
+      {isLoggedIn ? (
+        <HomePage />
+      ) : (
+        <div className="loginNavbar">
+          <div className="loginLogo">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/5969/5969117.png"
+              alt="company Logo"
+            />
+            <h1 className="loginLogoDetails">
+              HLGS(Healthy Living Guide for Seniors)
+            </h1>
+          </div>
+          <div className="loginSection">
+            <div className="loginInput">
+              <label htmlFor="">Email</label>
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="loginInput">
+              <label htmlFor="">Password</label>
+              <input type="password" placeholder="Password" />
+            </div>
+            <button onClick={handleLogin}>Login</button>;
+          </div>
         </div>
-        <div className="loginInput">
-          <label htmlFor="">Password</label>
-          <input type="password" placeholder="Password" />
-        </div>
-        <button>Login</button>
-      </div>
+      )}
     </div>
   );
-}
+};
 
-export default LoginPage
+export default LoginPage;
