44865a4df69465f011b568eea27d1cfd13b50b46
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-08-09 22:50:22 +0530

    Updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header"
-import Aside from "./Aside"
+import Header from "../components/Header"
+import Aside from "../components/Aside"
 
 const HomePage = () => {
   return (
